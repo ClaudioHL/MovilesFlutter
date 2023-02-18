bc8a2beaba799e6f89320778a1e8f1f09392561f
ClaudioHL21 <claudioipad21@outlook.com>
2023-02-17 18:25:07 -0600

    Adding README

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,21 +1,35 @@
+import 'dart:convert';
+
+import 'package:cripto_lists/data/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
+  var exchanges = jsonDecode(CRYPTO_EXCHANGES) as List;
   HomePage({Key? key}) : super(key: key);
 
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Crypto APP"),
       ),
-      body: Container(),
+      body: ListView.separated(
+        itemCount: exchanges.length,
+        separatorBuilder: (BuildContext context, int index) {
+          return Divider();
+        },
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text("${exchanges[index]["name"]}"),
+            leading: Image.network("${exchanges[index]["image"]}"),
+            trailing: CircleAvatar(
+              backgroundColor: Colors.black,
+              child: Text("${exchanges[index]["trust_score"]}", style: TextStyle(color: Colors.white),)),
+            subtitle: Text("${exchanges[index]["country"]} \n ${exchanges[index]["year_established"]}"),
+          );
+        },
+      ),
     );
   }
 }
